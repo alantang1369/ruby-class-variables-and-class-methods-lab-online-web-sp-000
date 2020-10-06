c1d3655712216b7d4214ac09c0363ae110ae8b4b
Alan Tang <giabao_tang@yahoo.com>
2020-10-06 04:14:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song 
+  @@count = 0
   attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-  end
-  def initialize
+  
     @@count += 1
   end
   
